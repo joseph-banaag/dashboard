5cb8cc5b94f09d80506592aebb24dbf71b17e9ce
joseph banaag <josephrbanaag51@gmail.com>
2026-09-21 14:20:06 +0800

    saving some changes to the file

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -18,14 +18,14 @@ export default async function CardWrapper() {
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      />
+      /> */}
     </>
   );
 }
@@ -52,6 +52,7 @@ export function Card({
           truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
+        
       </p>
     </div>
   );

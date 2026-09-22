9d72a40bad17a4b8da129bafaaf612673c15cf08
joseph banaag <josephrbanaag51@gmail.com>
2026-09-22 09:49:03 +0800

    adding suspense fallback to the revenue chart and separate the fetching from the rest of the dashboard content overview allowing other contents to load first and then the chart info after.

---
+++ app/dashboard/(market)/market-two/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function MarketTwo() {
+  return (
+    <div>MarketTwo</div>
+  )
+}
+                                                                                                                                         
